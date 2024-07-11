139ff8a705f79d979e722696582e175061783cf1
P.S. AKSHAJ <p.s.akshaj@PSs-MacBook-Air.local>
2024-07-11 14:55:06 +0530

    update: remove vishal

--- DOMAIN SPECIFIC TASK-DEVOPS/test-3/create_csv.sh
+++ DOMAIN SPECIFIC TASK-DEVOPS/test-3/create_csv.sh
@@ -5,6 +5,5 @@ john.doe,devops,755
 jane.doe,developers,700
 alice.smith,qa,750
 bob.johnson,marketing,710
-vishal,marketing,755
 EOF
 echo "usernames.csv file created successfully."
--- DOMAIN SPECIFIC TASK-DEVOPS/test-3/manage_users.sh
+++ DOMAIN SPECIFIC TASK-DEVOPS/test-3/manage_users.sh
@@ -6,15 +6,15 @@ fi
 while IFS=',' read username group permission
 do    
     dscl . -create "/Users/$username"
-    mkdir "/Users/$username"
+    mkdir -p  "/Users/$username"
     if ! dscl . -read "/Groups/$group" >/dev/null 2>&1; then
         dscl . -create "/Groups/$group"
     fi
     dscl . -append "/Groups/$group" GroupMembership "$username"
     chmod "$permission" "/Users/$username"
-    mkdir "/Users/$username/projects"
+    mkdir -p  "/Users/$username/projects"
     echo "Welcome, $username! This is your projects directory." > "/Users/$username/projects/README.md"
-    chown -R "$username:$group" "/Users/$username/projects"
+    chown -R "$username:$group" "/Users/$username"
     echo "User $username created successfully with group $group and permission $permission"
 done < usernames.csv
 echo "User management process completed"
